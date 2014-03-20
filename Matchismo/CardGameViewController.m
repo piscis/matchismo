07383d407b88238f3d7debbf3b6c338be7b215af
Alexander Pirsig <apirsig@web.de>
2014-03-20 07:43:13 +0100

    flip count label

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -9,10 +9,19 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
 @end
 
 @implementation CardGameViewController
+
+
+-(void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d",self.flipCount];
+    NSLog(@"flipCount changed to %d",self.flipCount);
+}
 
 - (IBAction)touchCardButton:(UIButton *)sender
 {
@@ -20,11 +29,13 @@
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
                       forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
-    }else{
+    } else {
         [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
                           forState:UIControlStateNormal];
         [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
     }
+    
+    self.flipCount++;
 }
 
 @end
