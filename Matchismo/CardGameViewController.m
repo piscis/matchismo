2f99e0ca5d6f6508103682b2235b9c59b5b02a88
Alexander Pirsig <apirsig@web.de>
2014-03-20 07:31:57 +0100

    flipping logic

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -14,16 +14,17 @@
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
+- (IBAction)touchCardButton:(UIButton *)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                      forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    }else{
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+    }
 }
 
 @end
